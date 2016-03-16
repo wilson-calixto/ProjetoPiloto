8a7893e1c7a679a551af2a4471cca38b9e1ad5e9
wilosncalisto <wccj.snf@uea.edu.br>
2016-03-15 20:52:13 -0400

    noova

---
+++ Aluno/Aluno.java
@@ -0,0 +1,23 @@
+package monitoria;
+
+public class Aluno {
+	
+	String nome;
+	int matricula;
+	String atividade;
+	
+	void insereNome(String NomeDoAluno){
+		nome=NomeDoAluno;	
+	}
+	
+	void insereMatricula(int NumeroDaMatricula){
+		matricula=NumeroDaMatricula;
+	}
+	
+	void insereAtividade(String AtividadeDoAluno){
+		atividade=AtividadeDoAluno;
+		
+	}
+}
+
+
