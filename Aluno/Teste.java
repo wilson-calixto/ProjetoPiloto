4082e91430425b7dcae42668ce5618b88beaad68
wilsoncalisto <wilsoncalisto@users.noreply.github.com>
2016-03-22 19:47:20 -0400

    Update Teste.java
    main

--- Aluno/Teste.java
+++ Aluno/Teste.java
@@ -13,3 +13,61 @@ public class Teste {
 	}
 }
 		
+/*
+
+
+
+package monitoria;
+
+import javax.swing.JOptionPane;
+
+
+
+public class MonitoriaPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Aluno[] turma=new Aluno[2];
+		
+		
+		
+		
+		for(int i=0;i<turma.length;i++)
+		{
+					Aluno aluno = new Aluno();
+					String nome = JOptionPane.showInputDialog("digite o nome ");
+					aluno.insereNome(nome);
+					String matricula = JOptionPane.showInputDialog("digite o matricula ");
+					int m = Integer.parseInt(matricula);
+					
+					aluno.insereMatricula(m);
+					turma[i]=aluno;
+			
+		}
+		
+		String listaDealunos="-----------Lista de alunos------------\n";
+		
+		for(int i =0; i<turma.length;i++ ){
+			listaDealunos+="Nome do aluno:   "+
+			turma[i].getNome()+"\n"+"matricula do aluno:   "+
+			turma[i].getMatricula()+"\n";
+			
+			System.out.println("Obrigado pela comtribuicao "+turma[i].matricula+ " volte sempre ");
+			
+		}
+		JOptionPane.showMessageDialog(null,listaDealunos);
+		
+		
+		
+
+	}
+
+}
+
+
+
+
+
+
+*/
