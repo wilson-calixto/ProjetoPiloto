e252bd916c70665c302381eb398473146aec740a
wilsoncalisto <wilsoncalisto@users.noreply.github.com>
2016-03-31 18:34:10 -0400

    Update BuscaSequencial.cpp
    id

--- nova/BuscaSequencial.cpp
+++ nova/BuscaSequencial.cpp
@@ -1,46 +1,57 @@
-#include<iostream>
-#include<cstdlib>
-#define TAM 10
+package avaliacao;
 
 
-using namespace std;
-// colocar variaveis inicio e fim
-int BuscaSequencial(int *vet,int pivo,int procurado)
-{
-	pivo=pivo/2;
 
+public class Conta {
 
-	if(procurado==vet[pivo])
-	{
-		return pivo;
-	}
-
-	if(pivo==0 && procurado!=vet[pivo])
-	{
-		return -1;
-	}else{
-		
-		BuscaSequencial(vet,pivo-1,procurado);	
-		BuscaSequencial(vet,pivo+1,procurado);	
-	}	
-}
-int main(){	
-	int n[TAM],indice=TAM,i,procurado=3;
-	for(i=9;i>=0;i--)
-	{
-		n[i]=i;
-//		cin>>n[i];
-
-	}
-
-	for(i=0;i<TAM;i++)
-	{
-		cout<<n[i]<<"\n";
-
-	}
-	cout<<"resuldado indice \n"<<BuscaSequencial(n,indice-1,procurado)<<"";	
-
+	private String tipo;
+	private int valor,codigo;
+	private String status;
 	
-	return 0;
-}
+	
+	Conta(){}
 
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public int getValor() {
+		return valor;
+	}
+
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+	
+}
