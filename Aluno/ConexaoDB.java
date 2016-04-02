3672ad3405dd78a6d9f5bed25f45988fa7a03140
luizfernandobrandao <lf_brandao@yahoo.com>
2016-04-01 22:21:28 -0400

    Update classes

--- Aluno/ConexaoDB.java
+++ Aluno/ConexaoDB.java
@@ -1,27 +1,73 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+//import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConexaoDB {
-
+	
 	final String driver = "com.mysql.jdbc.Driver";
-	final String url = "jdbc:mysql://localhost:3306/";
+	final String url = "jdbc:mysql://localhost:3306/alunos";
+	String INSERT_QUERY = "INSERT INTO alunos(nome, matricula, atividade) VALUES(?, ?, ?)";
+	String SELECT_QUERY = "SELECT * FROM alunos WHERE matricula = ";
+	String DELETE_QUERY = "DELETE FROM alunos WHERE matricula = ?";
+	
+	public void insert(String nome, int matricula, String atividade) {
+		try {
 
-	public ConexaoDB (String db, String usuario, String senha) {
-
-      	try {
-
-        		Class.forName (driver).newInstance ();
-			Connection conexao = DriverManager.getConnection (url+db, usuario, senha);
+        	Class.forName (driver).newInstance ();
+			Connection conexao = DriverManager.getConnection (url, "root", "banco123");
+			PreparedStatement preparedStmt = conexao.prepareStatement(INSERT_QUERY);
+		    preparedStmt.setString (1, nome);
+		    preparedStmt.setInt(2, matricula);
+		    preparedStmt.setString (3, atividade);
+		    preparedStmt.execute();
 			conexao.close ();
 
-        	} catch (SQLException e) {
+        } catch (Exception ex) {
+            	ex.printStackTrace();
+        }
+	}
+	
+	public List<Aluno> select(int matricula) {
+		ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+		try {
 
-            	throw new RuntimeException (e);
+        	Class.forName (driver).newInstance ();
+			Connection conexao = DriverManager.getConnection (url, "root", "banco123");
+			Statement statement = conexao.createStatement();
+			ResultSet resultSet = statement.executeQuery(SELECT_QUERY+matricula);
+			while (resultSet.next())
+			{
+				Aluno aluno = new Aluno();
+				
+				aluno.insereNome(resultSet.getString("nome"));
+				aluno.insereMatricula(resultSet.getInt("matricula"));
+				aluno.insereAtividade(resultSet.getString("atividade"));
+				alunos.add(aluno);
+			}
+
+        } catch (Exception ex) {
+            	ex.printStackTrace();
+        }
+		return alunos;
+	}
+	
+	public void delete(int matricula) {
+		try {
+
+			Class.forName (driver).newInstance ();
+			Connection conexao = DriverManager.getConnection (url, "root", "banco123");
+			PreparedStatement preparedStmt = conexao.prepareStatement(DELETE_QUERY);
+			preparedStmt.setInt(2, matricula);
+		    preparedStmt.execute();
+			conexao.close ();
 
 		} catch (Exception ex) {
-            	throw new RuntimeException (ex);
-        	}
+	      	ex.printStackTrace();
+	    }
 	}
 }
-
--- Aluno/Janela.java
+++ Aluno/Janela.java
@@ -1,57 +1,112 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Janela extends JFrame {
 
 	private JButton button;
+	private JButton button2;
 	private JTextField textField1;
 	private JTextField textField2;
 	private JTextField textField3;
+	private JTextField textField4;
 	private JLabel label1;
 	private JLabel label2;
 	private JLabel label3;
+	private JTable table;
+	private DefaultTableModel model = new DefaultTableModel();
 
 	public Janela() {
 
-		super( "Projeto Piloto" ); 
-		setLayout(new GridBagLayout());
+		super( "Projeto Piloto" );
+		JTabbedPane tabbedPane = new JTabbedPane();
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(3, 3, 3, 3);
 		gbc.gridy = 0; 
-	    	gbc.gridx = 0; 
-	    	gbc.gridwidth = 2;
-	    	gbc.anchor = GridBagConstraints.WEST;
+	    gbc.gridx = 0; 
+	    gbc.gridwidth = 2;
+	    gbc.anchor = GridBagConstraints.WEST;
 		label1 = new JLabel();
 		label1.setText( "Nome" );
-		add( label1, gbc );
+		panel1.add( label1, gbc );
+		Handler handler = new Handler();
 		gbc.gridy = 0; 
-	    	gbc.gridx = 2; 
-	    	gbc.gridwidth = 1;
+	    gbc.gridx = 2; 
+	    gbc.gridwidth = 1;
 		textField1 = new JTextField(30);
-		add( textField1, gbc );
+		textField1.addActionListener(handler);
+		panel1.add( textField1, gbc );
 		gbc.gridy = 1;
-	    	gbc.gridx = 0;
+	    gbc.gridx = 0;
 		label2 = new JLabel();
 		label2.setText( "Matrícula" );
-		add( label2, gbc );
+		panel1.add( label2, gbc );
 		gbc.gridy = 1;
-	    	gbc.gridx = 2;
+	    gbc.gridx = 2;
 		textField2 = new JTextField(30);
-		add( textField2, gbc );
+		textField2.addActionListener(handler);
+		panel1.add( textField2, gbc );
 		gbc.gridy = 2;
-	    	gbc.gridx = 0;
+	    gbc.gridx = 0;
 		label3 = new JLabel();
 		label3.setText( "Atividade" );
-		add( label3, gbc );
+		panel1.add( label3, gbc );
 		gbc.gridy = 2;
-	    	gbc.gridx = 2;
+	    gbc.gridx = 2;
 		textField3 = new JTextField(30);
-		add( textField3, gbc );
+		textField3.addActionListener(handler);
+		panel1.add( textField3, gbc );
 		gbc.gridy = 3;
-	    	gbc.gridx = 2;
+	    gbc.gridx = 2;
 		button = new JButton( "Cadastrar" );
-		add( button, gbc );
+		button.addActionListener(handler);
+		panel1.add( button, gbc );
+		tabbedPane.addTab("Cadastro", null, panel1);
+		
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(new FlowLayout());
+		textField4 = new JTextField(30);
+		panel2.add(textField4);
+		button2 = new JButton("Pesquisar");
+		button2.addActionListener(handler);
+		panel2.add(button2);
+		table = new JTable(model);
+		model.addColumn("Nome");
+		model.addColumn("Matrícula");
+		model.addColumn("Atividade");
+		table.getColumnModel().getColumn(0).setPreferredWidth(160);
+		table.getColumnModel().getColumn(1).setPreferredWidth(160);
+		table.getColumnModel().getColumn(2).setPreferredWidth(160);
+		panel2.add(table);
+		tabbedPane.addTab("Pesquisa", panel2);
+		add(tabbedPane);
+	}
+	
+	public class Handler implements ActionListener {
+		
+		ConexaoDB conexao = new ConexaoDB ();
+		Aluno aluno = new Aluno();
+		
+		@Override
+		public void actionPerformed(ActionEvent event) {
+		
+			if(event.getSource() == button) {
+				aluno.insereNome(textField1.getText());
+				aluno.insereMatricula(Integer.parseInt(textField2.getText()));
+				aluno.insereAtividade(textField3.getText());
+				conexao.insert(aluno.getNome(), aluno.getMatricula(), aluno.getAtividade());
+				
+			}
+			
+			if(event.getSource() == button2) {
+				for(Aluno a : conexao.select(Integer.parseInt(textField4.getText())))
+					model.addRow(new Object[]{a.getNome(), a.getMatricula(), a.getAtividade()});
+				
+			}
+		}
 	}
 }
-
-	
