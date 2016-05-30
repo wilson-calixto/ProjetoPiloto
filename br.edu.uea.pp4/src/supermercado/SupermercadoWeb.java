d0f27299e1e78b5ed4f2ac282a43ce170d8d2076
wilsoncalisto <wilsoncalisto@users.noreply.github.com>
2016-05-30 11:08:09 -0400

    Update SupermercadoWeb.java
    classe refatorada

--- br.edu.uea.pp4/src/supermercado/SupermercadoWeb.java
+++ br.edu.uea.pp4/src/supermercado/SupermercadoWeb.java
@@ -1,11 +1,11 @@
 package supermercado;
 
 import estoque.*;
-import java.util.*;
+import produtos.Genero;
+import produtos.Marca;
+import produtos.Produto;
 
-/**
- * 
- */
+import java.util.*;
 public class SupermercadoWeb {
 
     private Estoque estoque;
@@ -14,55 +14,31 @@ public class SupermercadoWeb {
     private ArrayList<Produto> produtos;
     private ArrayList<Marca> marcas;
     
-    public SupermercadoWeb() {
+   /* public SupermercadoWeb() {
         iniciaSupermercado();
     }
-
-    private void iniciaSupermercado() {
+*/
+    public void iniciaSupermercado() {
         estoque = new Estoque();
     }
 
-
-
-
-    /**
-     * @return
-     */
-    private void iniciaSupermercado() {
+    public ArrayList<Marca> getMarcas() {
         // TODO implement here
-        
+        return marcas;
     }
 
-    /**
-     * @return
-     */
-    public List getMarcas() {
+    public ArrayList<Produto> getProdutos() {
         // TODO implement here
-        return null;
+        return  produtos;
     }
 
-    /**
-     * @return
-     */
-    public List getProdutos() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @return
-     */
     public Estoque getEstoque() {
         // TODO implement here
-        return null;
+        return estoque;
     }
-
-    /**
-     * @return
-     */
     public Carrinho getCarrinho() {
         // TODO implement here
-        return null;
+        return carrinho;
     }
 
 }
